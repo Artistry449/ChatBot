a62da3b6daa373ee7dc346d712ee616231bd5b17
Artistry449 <artistry449@gmail.com>
2024-06-07 15:59:04 +0800

    added webhook functionality

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -11,20 +11,25 @@ const client = StreamChat.getInstance(api_key, secret, app_id);
 const controller = require("./choiceController");
 const answerController = require("./answerController");
 
-const createChannel = async () => {
-    const channel = client.channel("messaging", {
-        members: ["limebot", "1"],
-        created_by_id: "limebot"
-    });
+// const createChannel = async () => {
+//     const channel = client.channel("messaging", {
+//         members: ["limebot", "1"],
+//         created_by_id: "limebot"
+//     });
 
-    try {
-        console.log("Creating channel...");
-        await channel.create();
-        console.log("Channel created successfully.");
-    } catch (error) {
-        console.error("Error creating channel:", error);
-    }
+//     try {
+//         console.log("Creating channel...");
+//         await channel.create();
+//         console.log("Channel created successfully.");
+//     } catch (error) {
+//         console.error("Error creating channel:", error);
+//     }
 
+//     return channel;
+// }
+
+const joinChannel = async (id) => {
+    const channel = client.channel("messaging", id);
     return channel;
 }
 
@@ -47,17 +52,16 @@ const sendMessage = async (channel, text, attachments, user_id) => {
 exports.webhookHandler = async (req, res) => {
     const parent_id = req.params.id;
     // CREATE CHANNEL
-    const channel = await createChannel();
+    // const channel = await createChannel();
+    const channel = await joinChannel("SERVER_TEST_HHE");
     // WATCH CHANNEL
     const state = await watchChannel(channel);
-    // SEND MESSAGE
-
-    // console.log(state.messages[state.messages.length - 1].text);
 
     // CHECKING IF EVENT IS "MESSAGE.NEW" AND ID IS PROVIDED
     if (parent_id && req.body.type === "message.new") {
         let choice = await controller.getChoice(parent_id);
 
+        // CHOICE Байхгүй үед ANSWER table-ээс үр дүн буцаана
         if (choice.length === 0) {
             const answer = await answerController.getAnswer(parent_id);
             return res.status(200).json({
@@ -69,10 +73,12 @@ exports.webhookHandler = async (req, res) => {
         }
         console.log(choice);
 
+        // SENDING MESSAGE TO USER FROM DATABASE
         for (let i = 0; i < choice.length; i++) {
             const message = await sendMessage(channel, choice[i].choice_content, [], "limebot");
         }
 
+        // CHOICE байгаа үед choice буцаана
         return res.status(200).json({
             status: "success",
             data: {
